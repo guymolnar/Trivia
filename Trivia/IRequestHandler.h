9d38b23fad314252b5de2a4926598c764cb7e2fb
guymo <guymolnar2000@gmail.com>
2026-04-17 22:49:39 +0300

    Added requests structs and functino declerations in IRequestHandler.h

--- Trivia/IRequestHandler.h
+++ Trivia/IRequestHandler.h
@@ -1,4 +1,19 @@
 #pragma once
-class IRequestHandler {
+#include <vector>
 
+struct RequestInfo {
+	uint8_t id;
+	time_t receivalTime;
+	std::vector<uint8_t> buffer;
+};
+
+struct RequestResult {
+	std::vector<uint8_t> buffer;
+	IRequestHandler* newHandler;
+};
+
+class IRequestHandler {
+public:
+	bool isRequestRelevant(const RequestInfo& requestInfo);
+	RequestResult handleRequest(const RequestInfo& requestInfo);
 };
