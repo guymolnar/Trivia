6b8d82fdd1ef750b30f1057f7280e673f4acf61a
guymo <guymolnar2000@gmail.com>
2026-04-17 22:58:58 +0300

    Added overrding functions declerations

--- Trivia/LoginRequestHandler.h
+++ Trivia/LoginRequestHandler.h
@@ -2,5 +2,7 @@
 #include "IRequestHandler.h"
 
 class LoginRequestHandler : public IRequestHandler {
-
+public:
+	virtual bool isRequestRelevant(const RequestInfo& requestInfo) override;
+	virtual RequestResult handleRequest(const RequestInfo& requestInfo) override;
 };
