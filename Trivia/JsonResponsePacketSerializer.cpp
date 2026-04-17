7ed9d4907a64b2a03eaefc4b7bfb3344e70a1334
guymo <guymolnar2000@gmail.com>
2026-04-17 21:34:03 +0300

    Added serializeResponse (Login) function

--- Trivia/JsonResponsePacketSerializer.cpp
+++ Trivia/JsonResponsePacketSerializer.cpp
@@ -1,2 +1,18 @@
 #include "JsonResponsePacketSerializer.h"
 
+std::vector<uint8_t> serializeResponse(LoginResponse)
+{
+	json j = { {"status", 1} };
+	std::string jsonStr = j.dump();
+
+	std::vector<uint8_t> packet;
+	packet.push_back(1); // loginResponse code
+
+	uint32_t len = htonl(jsonStr.size()); //converting endianess to be the same as the network standard
+	uint8_t * lenBytes = reinterpret_cast<uint8_t*>(&len);
+
+	packet.insert(packet.end(), lenBytes, lenBytes + 4);
+	packet.insert(packet.end(), jsonStr.begin(), jsonStr.end());
+	
+	return packet;
+}
