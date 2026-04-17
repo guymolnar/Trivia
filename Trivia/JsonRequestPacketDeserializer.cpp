f159dab6b764be3a05264d55c609701c65e973cc
guymo <guymolnar2000@gmail.com>
2026-04-17 23:26:46 +0300

    Added signup request deseralizing function implementation

--- Trivia/JsonRequestPacketDeserializer.cpp
+++ Trivia/JsonRequestPacketDeserializer.cpp
@@ -11,3 +11,16 @@ LoginRequest JsonRequestPacketDeserializer::deserializeLoginRequest(std::vector<
 	LoginRequest info = { username, password };
 	return info;
 }
+
+SignupRequest JsonRequestPacketDeserializer::deserializeSignupRequest(std::vector<uint8_t> buffer)
+{
+	std::string jsonStr = std::string(buffer.begin(), buffer.end());
+	json j = json::parse(jsonStr);
+
+	std::string username = j.at("username");
+	std::string password = j.at("password");
+	std::string mail = j.at("mail");
+
+	SignupRequest info = { username, password, mail };
+	return info;
+}
