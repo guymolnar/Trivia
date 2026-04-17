019266851aaf6bd82937cf3da2dd52ecb30c1840
guymo <guymolnar2000@gmail.com>
2026-04-17 21:38:14 +0300

    Added serializeResponse (Error) function and scoped the functions to the class

--- Trivia/JsonResponsePacketSerializer.cpp
+++ Trivia/JsonResponsePacketSerializer.cpp
@@ -1,6 +1,6 @@
 #include "JsonResponsePacketSerializer.h"
 
-std::vector<uint8_t> serializeResponse(LoginResponse)
+std::vector<uint8_t> JsonResponsePacketSerializer::serializeResponse(LoginResponse)
 {
 	json j = { {"status", 1} };
 	std::string jsonStr = j.dump();
@@ -17,7 +17,7 @@ std::vector<uint8_t> serializeResponse(LoginResponse)
 	return packet;
 }
 
-std::vector<uint8_t> serializeResponse(SignupResponse)
+std::vector<uint8_t> JsonResponsePacketSerializer::serializeResponse(SignupResponse)
 {
 	json j = { {"status", 1} };
 	std::string jsonStr = j.dump();
@@ -33,3 +33,21 @@ std::vector<uint8_t> serializeResponse(SignupResponse)
 
 	return packet;
 }
+
+std::vector<uint8_t> JsonResponsePacketSerializer::serializeResponse(ErrorResponse)
+{
+	json j = { {"message", "ERROR"}};
+	std::string jsonStr = j.dump();
+
+	std::vector<uint8_t> packet;
+	packet.push_back(3); // loginResponse code
+
+	uint32_t len = htonl(jsonStr.size()); //converting endianess to be the same as the network standard
+	uint8_t* lenBytes = reinterpret_cast<uint8_t*>(&len);
+
+	packet.insert(packet.end(), lenBytes, lenBytes + 4);
+	packet.insert(packet.end(), jsonStr.begin(), jsonStr.end());
+
+	return packet;
+}
+
