263d4a6a83c100138e18403e00cc59995a46e9d3
guymo <guymolnar2000@gmail.com>
2026-04-19 16:52:25 +0300

    Added loginmanger c'tor and d'tor

--- Trivia/LoginManager.h
+++ Trivia/LoginManager.h
@@ -4,6 +4,9 @@
 #include "LoggedUser.h"
 class LoginManager {
 public:
+	LoginManager(IDatabase*);
+	~LoginManager();
+
 	void signup(std::string, std::string, std::string);
 	void login(std::string, std::string);
 	void logout(std::string);
