affb45e8456cf81f3f94767cb3fa98fb8b73d3ba
guymo <guymolnar2000@gmail.com>
2026-04-19 18:14:43 +0300

    Added login and signup functions to loginrequesthandler

--- Trivia/LoginRequestHandler.h
+++ Trivia/LoginRequestHandler.h
@@ -2,9 +2,14 @@
 #include "IRequestHandler.h"
 #include "JsonRequestPacketDeserializer.h"
 #include "JsonResponsePacketSerializer.h"
+#include "RequestHandlerFactory.h"
 
 class LoginRequestHandler : public IRequestHandler {
 public:
 	virtual bool isRequestRelevant(const RequestInfo& requestInfo) override;
 	virtual RequestResult handleRequest(const RequestInfo& requestInfo) override;
+private:
+	RequestHandlerFactory& m_handlerFactory;
+	RequestResult login(RequestInfo);
+	RequestResult signup(RequestInfo);
 };
