3b98fcda24a1c268f2dd52ed83b98008c085a284
guymo <guymolnar2000@gmail.com>
2026-04-19 17:04:31 +0300

    Added login function

--- Trivia/LoginManager.cpp
+++ Trivia/LoginManager.cpp
@@ -18,3 +18,16 @@ void LoginManager::signup(std::string name, std::string password, std::string ma
     }
     m_database->addNewUser(name, password, mail);
 }
+
+void LoginManager::login(std::string name, std::string password)
+{
+    if (!m_database->doesUserExist(name))
+    {
+        throw std::exception("User does not exist");
+    }
+    if (!m_database->doesPasswordMatch(name, password))
+    {
+        throw std::exception("Wrong password");
+    }
+    m_loggedUser.push_back(LoggedUser(name));
+}
