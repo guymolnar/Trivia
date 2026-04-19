90ecbddcdd3d1b111c03facf6dc0c6b25b007392
guymo <guymolnar2000@gmail.com>
2026-04-19 16:30:03 +0300

    Created sqlitedatabase class header

---
+++ Trivia/SqliteDataBase.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "IDatabase.h"
+#include "sqlite3.h"
+#include <string>
+
+class SqliteDataBase : public IDatabase {
+public:
+    SqliteDataBase();
+    ~SqliteDataBase();
+    bool open() override;
+    bool close() override;
+    int doesUserExist(std::string username) override;
+    int doesPasswordMatch(std::string username, std::string password) override;
+    int addNewUser(std::string username, std::string password, std::string email) override;
+
+private:
+    sqlite3* m_db;
+};
