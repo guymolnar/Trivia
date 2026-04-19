3a965f4b826e2de54d3ddec72761ea528f1c239d
guymo <guymolnar2000@gmail.com>
2026-04-19 14:15:30 +0300

    Added handlerRequest function

--- Trivia/LoginRequestHandler.cpp
+++ Trivia/LoginRequestHandler.cpp
@@ -4,3 +4,11 @@ bool LoginRequestHandler::isRequestRelevant(const RequestInfo& requestInfo)
 {
 	return (requestInfo.id == LOGIN_REQUEST_CODE);
 }
+
+RequestResult LoginRequestHandler::handleRequest(const RequestInfo& requestInfo)
+{
+	RequestResult requestResult;
+	requestResult.buffer = requestInfo.buffer;
+	requestResult.newHandler = new LoginRequestHandler();
+	return requestResult;
+}
