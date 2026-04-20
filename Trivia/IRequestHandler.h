e4ed87daa5b84db98c16a50a8cc82892e9db9b37
guymo <guymolnar2000@gmail.com>
2026-04-20 22:15:20 +0300

    Added irequesthandler virtual d'tor

--- Trivia/IRequestHandler.h
+++ Trivia/IRequestHandler.h
@@ -18,6 +18,7 @@ struct RequestResult {
 
 class IRequestHandler {
 public:
+	virtual ~IRequestHandler() = default;
 	virtual bool isRequestRelevant(const RequestInfo& requestInfo) = 0;
 	virtual RequestResult handleRequest(const RequestInfo& requestInfo) = 0;
 };
