9e675cb2b89faf458d1eb23d62084ceeebea9cae
guymo <guymolnar2000@gmail.com>
2026-04-20 16:32:32 +0300

    Added LoginRequestHandler c'tor

--- Trivia/LoginRequestHandler.cpp
+++ Trivia/LoginRequestHandler.cpp
@@ -1,5 +1,11 @@
 #include "LoginRequestHandler.h"
 
+LoginRequestHandler::LoginRequestHandler(RequestHandlerFactory& factory) : m_handlerFactory(factory) 
+{
+
+}
+
+
 bool LoginRequestHandler::isRequestRelevant(const RequestInfo& requestInfo)
 {
 	return (requestInfo.id == LOGIN_REQUEST_CODE);
@@ -8,9 +14,8 @@ bool LoginRequestHandler::isRequestRelevant(const RequestInfo& requestInfo)
 RequestResult LoginRequestHandler::handleRequest(const RequestInfo& requestInfo)
 {
 	LoginRequest loginRequest = JsonRequestPacketDeserializer::deserializeLoginRequest(requestInfo.buffer);
-
 	LoginResponse loginResponse;
-	loginResponse.status = 1;
+	loginResponse.status = requestInfo.id;
 
 	RequestResult requestResult;
 	requestResult.buffer = JsonResponsePacketSerializer::serializeResponse(loginResponse);
