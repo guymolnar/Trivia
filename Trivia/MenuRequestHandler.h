b65c2552cd3900d53684cfeba6166bb1154a64b1
guymo <guymolnar2000@gmail.com>
2026-04-20 17:20:30 +0300

    Added virtual functions declerations to menurequesthandler

--- Trivia/MenuRequestHandler.h
+++ Trivia/MenuRequestHandler.h
@@ -4,4 +4,6 @@
 class MenuRequestHandler : public IRequestHandler{
 public:
 	MenuRequestHandler(RequestHandlerFactory& handlerFactory);
+	bool isRequestRelevant(const RequestInfo& requestInfo) override;
+	RequestResult handleRequest(const RequestInfo& requestInfo) override;
 };
