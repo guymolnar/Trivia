e8e5950229808944e52bd50a39841397b6aaea1e
guymo <guymolnar2000@gmail.com>
2026-04-21 19:35:02 +0300

    Added getRoom and getRoomStatus functions

--- Trivia/RoomManager.cpp
+++ Trivia/RoomManager.cpp
@@ -11,3 +11,23 @@ void RoomManager::deleteRoom(roomID ID)
 {
     m_rooms.erase(ID);
 }
+
+Room* RoomManager::getRoom(roomID ID)
+{
+    auto it = m_rooms.find(ID);
+    if (it == m_rooms.end())
+    {
+        return nullptr;
+    }
+    return &it->second;
+}
+
+RoomStatus RoomManager::getRoomState(roomID ID)
+{
+    Room* r = getRoom(ID);
+    if (!r)
+    {
+        throw std::exception("Room does not exist");
+    }
+    return r->getRoomStatus();
+}
