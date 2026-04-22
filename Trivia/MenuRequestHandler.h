c7e1b143783cc5d6cb78884492eabb74d6c85cab
guymo <guymolnar2000@gmail.com>
2026-04-22 22:28:52 +0300

    Added new functions and added back the m_handlerFactory

--- Trivia/MenuRequestHandler.h
+++ Trivia/MenuRequestHandler.h
@@ -5,8 +5,17 @@ class RequestHandlerFactory;
 class MenuRequestHandler : public IRequestHandler{
 public:
 	MenuRequestHandler(RequestHandlerFactory& handlerFactory);
+	~MenuRequestHandler() = default;
 	bool isRequestRelevant(const RequestInfo& requestInfo) override;
 	RequestResult handleRequest(const RequestInfo& requestInfo) override;
 private:
 	LoggedUser m_user;
+	RequestHandlerFactory& m_handlerFactory;
+	RequestResult signout(const RequestInfo& requestInfo);
+	RequestResult getRooms(const RequestInfo& requestInfo);
+	RequestResult getPlayersInRoom(const RequestInfo& requestInfo);
+	RequestResult getPersonalStats(const RequestInfo& requestInfo);
+	RequestResult getHighScore(const RequestInfo& requestInfo);
+	RequestResult joinRoom(const RequestInfo& requestInfo);
+	RequestResult createRoom(const RequestInfo& requestInfo);
 };
