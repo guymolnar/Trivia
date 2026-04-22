8c6ad1303294a2c29c940e0b6d77f051eb7e7cd5
guymo <guymolnar2000@gmail.com>
2026-04-22 18:28:28 +0300

    Created LogoutResponse serializer function

--- Trivia/JsonResponsePacketSerializer.cpp
+++ Trivia/JsonResponsePacketSerializer.cpp
@@ -6,7 +6,7 @@ std::vector<uint8_t> JsonResponsePacketSerializer::serializeResponse(const Login
 	std::string jsonStr = j.dump();
 
 	std::vector<uint8_t> packet;
-	packet.push_back(LOGIN_REQUEST_CODE);
+	packet.push_back(LOGIN_RESPONSE_CODE);
 
 	uint32_t len = htonl(jsonStr.size()); //converting endianess to be the same as the network standard
 	uint8_t * lenBytes = reinterpret_cast<uint8_t*>(&len);
@@ -23,7 +23,7 @@ std::vector<uint8_t> JsonResponsePacketSerializer::serializeResponse(const Signu
 	std::string jsonStr = j.dump();
 
 	std::vector<uint8_t> packet;
-	packet.push_back(SIGNUP_REQUEST_CODE);
+	packet.push_back(SIGNUP_RESPONSE_CODE);
 
 	uint32_t len = htonl(jsonStr.size()); //converting endianess to be the same as the network standard
 	uint8_t* lenBytes = reinterpret_cast<uint8_t*>(&len);
@@ -40,7 +40,24 @@ std::vector<uint8_t> JsonResponsePacketSerializer::serializeResponse(const Error
 	std::string jsonStr = j.dump();
 
 	std::vector<uint8_t> packet;
-	packet.push_back(ERROR_REQUEST_CODE);
+	packet.push_back(ERROR_RESPONSE_CODE);
+
+	uint32_t len = htonl(jsonStr.size()); //converting endianess to be the same as the network standard
+	uint8_t* lenBytes = reinterpret_cast<uint8_t*>(&len);
+
+	packet.insert(packet.end(), lenBytes, lenBytes + 4);
+	packet.insert(packet.end(), jsonStr.begin(), jsonStr.end());
+
+	return packet;
+}
+
+std::vector<uint8_t> JsonResponsePacketSerializer::serializeResponse(const LogoutResponse& response)
+{
+	json j = { {"status", response.status} };
+	std::string jsonStr = j.dump();
+
+	std::vector<uint8_t> packet;
+	packet.push_back(LOGOUT_RESPONSE_CODE);
 
 	uint32_t len = htonl(jsonStr.size()); //converting endianess to be the same as the network standard
 	uint8_t* lenBytes = reinterpret_cast<uint8_t*>(&len);
