37b0cf490fc943ec5d1035d6dd930ed5c512f11f
guymo <guymolnar2000@gmail.com>
2026-04-22 17:21:00 +0300

    Added getNumOfTotalAnswers function

--- Trivia/SqliteDataBase.cpp
+++ Trivia/SqliteDataBase.cpp
@@ -107,7 +107,13 @@ int SqliteDataBase::getNumOfCorrectAnswers(std::string username)
 
 int SqliteDataBase::getNumOfTotalAnswers(std::string username)
 {
-    return 0;
+    std::string query = "SELECT totalAnswers FROM statistics WHERE username = '" + username + "';";
+    int count = 0;
+    sqlite3_exec(m_db, query.c_str(), [](void* data, int, char** argv, char**) {
+        *reinterpret_cast<int*>(data) = std::stoi(argv[0]);
+        return 0;
+        }, &count, nullptr);
+    return count;
 }
 
 int SqliteDataBase::getNumOfPlayerGames(std::string username)
