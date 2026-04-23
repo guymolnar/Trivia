d693c51696ff6d68470a846496bad7dc1c23c55e
guymo <guymolnar2000@gmail.com>
2026-04-23 16:39:52 +0300

    Added room and statistics managers to requesthandlerfactory

--- Trivia/RequestHandlerFactory.cpp
+++ Trivia/RequestHandlerFactory.cpp
@@ -10,12 +10,17 @@ LoginRequestHandler* RequestHandlerFactory::createLoginRequestHandler()
 	return new LoginRequestHandler(*this);
 }
 
-MenuRequestHandler* RequestHandlerFactory::createMenuRequestHandler()
+MenuRequestHandler* RequestHandlerFactory::createMenuRequestHandler(LoggedUser user)
 {
-	return new MenuRequestHandler(*this);
+    return new MenuRequestHandler(*this, user);
 }
 
-LoginManager& RequestHandlerFactory::getLoginManager()
+RoomManager& RequestHandlerFactory::getRoomManager()
 {
-	return LoginManager::getInstance(m_database);
+    return RoomManager::getInstance();
+}
+
+StatisticsManager& RequestHandlerFactory::getStatisticsManager()
+{
+    return StatisticsManager::getInstance();
 }
