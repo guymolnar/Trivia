ada3f0b0ddb6d05b02d7617efd2eba9a370f4103
guymo <guymolnar2000@gmail.com>
2026-04-27 16:30:46 +0300

    Added join room and room lobby windows

---
+++ TriviaClient/RoomLobbyWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace TriviaClient
+{
+    public partial class RoomLobbyWindow : Window
+    {
+        private string _username;
+
+        public RoomLobbyWindow(string username, string roomName, string admin)
+        {
+            InitializeComponent();
+            _username = username;
+            txtRoomName.Text = roomName;
+            txtAdmin.Text = $"Admin: {admin}";
+            // TODO: load players from server
+        }
+
+        private void btnLeave_Click(object sender, RoutedEventArgs e)
+        {
+            HubWindow hub = new HubWindow(_username);
+            hub.Show();
+            this.Close();
+        }
+    }
+}
