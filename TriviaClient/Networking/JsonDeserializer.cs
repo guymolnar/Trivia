8491c227b9d5477ed0915e79f1a383a75f0d75ec
guymo <guymolnar2000@gmail.com>
2026-04-27 17:10:19 +0300

    Added json deserializer

---
+++ TriviaClient/Networking/JsonDeserializer.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaClient.Networking
+{
+    public static class JsonDeserializer
+    {
+        public static T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json)!; //Genuinly amazed by how simple this is in c#
+            //Theres a ! at the end because vs gave me an annoying warning that it can return NULL so i searched online how to fix it.
+        }
+    }
+}
