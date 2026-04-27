72764c12ff5c9ad4777a3cf96cda3f58a0e49fd9
guymo <guymolnar2000@gmail.com>
2026-04-27 16:53:50 +0300

    Added requests classes

---
+++ TriviaClient/Models/Requests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaClient.Models
+{
+ 
+    //Q: Why are there ?'s?
+    //A: because without it the vs warns that strings are non-nullable
+    //And it's kind of annoying
+
+    public class LoginRequest
+    {
+        public string? username;
+        public string? password;
+    }
+
+    public class SignupRequest
+    {
+        public string? username;
+        public string? password;
+        public string? email;
+        public string? address;
+        public string? phone;
+        public string? birthday;
+    }
+
+    public class GetPlayersInRoomRequest
+    {
+        public int roomId;
+    }
+
+    public class JoinRoomRequest
+    {
+        public int roomId;
+    }
+
+    public class CreateRoomRequest
+    {
+        public string? roomName;
+        public int maxUsers;
+        public int questionsCount;
+        public int answersTimeout;
+    }
+}
