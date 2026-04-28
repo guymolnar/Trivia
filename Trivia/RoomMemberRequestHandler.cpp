c143a1be1a7e4efc0dd4a7c97131c7cc9206b97f
guymo <guymolnar2000@gmail.com>
2026-04-28 18:50:57 +0300

    Made leaveRoom function remove user from the room

--- Trivia/RoomMemberRequestHandler.cpp
+++ Trivia/RoomMemberRequestHandler.cpp
@@ -38,8 +38,35 @@ RequestResult RoomMemberRequestHandler::leaveRoom(const RequestInfo& requestInfo
 {
     try
     {
+        Room* room = m_roomManager.getRoom(m_roomId);
+        if (!room)
+        {
+            throw std::exception("Room not found");
+        }
+        room->removeUser(m_user);
         LeaveRoomResponse response{ 1 };
         return { JsonResponsePacketSerializer::serializeResponse(response), nullptr };
+    }
+    catch (const std::exception& e)
+    {
+        ErrorResponse err{ e.what() };
+        return { JsonResponsePacketSerializer::serializeResponse(err), this };
+    }
+}
+
+RequestResult RoomMemberRequestHandler::getRoomState(const RequestInfo& requestInfo)
+{
+    try
+    {
+        Room* room = m_roomManager.getRoom(m_roomId);
+        if (!room)
+        {
+            throw std::exception("Room not found");
+        }
+        RoomData meta = room->getRoomMetadata();
+        std::vector<std::string> players = room->getAllUsers();
+        GetRoomStateResponse response{ 1, false, players, meta.numOfQuestions, meta.timePerQuestions };
+        return { JsonResponsePacketSerializer::serializeResponse(response), this };
     }
     catch (const std::exception& e)
     {
