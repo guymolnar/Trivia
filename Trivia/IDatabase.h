991672155931ec225641ce2ab55ea1f1d1b54a2f
guymo <guymolnar2000@gmail.com>
2026-04-29 21:51:36 +0300

    Added IDatabase default d'tor

--- Trivia/IDatabase.h
+++ Trivia/IDatabase.h
@@ -6,6 +6,7 @@ class IDatabase {
 public:
 	virtual bool open() = 0;
 	virtual bool close() = 0;
+	virtual ~IDatabase() = default;
 	virtual int doesUserExist(const std::string& username) = 0;
 	virtual int doesPasswordMatch(const std::string& username, const std::string& password) = 0;
 	virtual int addNewUser(const std::string& username, const std::string& password, const std::string& email, const std::string& address, const std::string& phone, const std::string& birthday) = 0;
