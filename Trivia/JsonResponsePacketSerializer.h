eb30347d780fb62347e6c54f0abaf3c9726edd77
guymo <guymolnar2000@gmail.com>
2026-04-29 21:57:56 +0300

    Created responses.h file

--- Trivia/JsonResponsePacketSerializer.h
+++ Trivia/JsonResponsePacketSerializer.h
@@ -5,71 +5,9 @@
 #include <winsock2.h>
 #include "IRequestHandler.h"
 #include "Room.h"
+#include "Responses.h"
 
 using json = nlohmann::json;
-struct LoginResponse {
-	unsigned int status;
-};
-
-struct SignupResponse {
-	unsigned int status;
-};
-
-struct ErrorResponse {
-	std::string message;
-};
-
-struct LogoutResponse {
-	unsigned int status;
-};
-
-struct GetRoomsResponse {
-	unsigned int status;
-	std::vector<RoomData> rooms;
-};
-
-struct GetPlayersInRoomResponse {
-	std::vector<std::string> players;
-};
-
-struct GetHighScoreResponse {
-	unsigned int status;
-	std::vector<std::string> statistics;
-};
-
-struct GetPersonalStatsResponse {
-	unsigned int status;
-	std::vector<std::string> statistics;
-};
-
-struct JoinRoomResponse {
-	unsigned int status;
-};
-
-struct CreateRoomResponse {
-	unsigned int status;
-	unsigned int roomId;
-};
-
-struct CloseRoomResponse {
-	unsigned int status;
-};
-
-struct StartGameResponse {
-	unsigned int status;
-};
-
-struct GetRoomStateResponse {
-	unsigned int status;
-	bool hasGameBegun;
-	std::vector<std::string> players;
-	unsigned int questionCount;
-	unsigned int answerTimeout;
-};
-
-struct LeaveRoomResponse {
-	unsigned int status;
-};
 
 class JsonResponsePacketSerializer {
 public:
