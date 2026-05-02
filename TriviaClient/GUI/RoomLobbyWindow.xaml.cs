7fd3aab53172339e7190a16d284101308f96fc61
guymo <guymolnar2000@gmail.com>
2026-05-02 14:28:10 +0300

    Made the thread sleep a second instead of 3

--- TriviaClient/GUI/RoomLobbyWindow.xaml.cs
+++ TriviaClient/GUI/RoomLobbyWindow.xaml.cs
@@ -65,7 +65,7 @@ namespace TriviaClient
             {
                 while (_refreshing)
                 {
-                    Thread.Sleep(3000);
+                    Thread.Sleep(1000);
                     if (!_refreshing) break;
                     lock (_socketLock)
                     {
